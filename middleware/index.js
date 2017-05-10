6bee0af20f7cf3da7e0f0ead988dcbe296137a2a
Роман <roman.sarder@yandex.ru>
2017-05-10 22:42:57 +0300

    Remove comments

--- middleware/index.js
+++ middleware/index.js
@@ -18,38 +18,16 @@ let validateRegisterInput = (req, res, next) => {
     }
 }
 let validateLoginInput = (req, res, next) => {
-        let email = req.body.email;
-        let password = req.body.password;
-        if (!validator.isEmail(email)) {
-            next(new Error('Invalid email'));
-        } else if (validator.isEmpty(password)) {
-            next(new Error('Password is required'))
-        } else {
-            next();
-        }
+    let email = req.body.email;
+    let password = req.body.password;
+    if (!validator.isEmail(email)) {
+        next(new Error('Invalid email'));
+    } else if (validator.isEmpty(password)) {
+        next(new Error('Password is required'))
+    } else {
+        next();
     }
-    // let isLogged = (req, res, next) => {
-    //     if (!req.body.token) {
-    //         let err = new Error('You must log in');
-    //         err.status = 401;
-    //         next(err);
-    //     }
-    //     jwt.verify(req.body.token, secret, (err, decoded) => {
-    //         if (err) {
-    //             next(new Error('Invalid token provided'));
-    //         } else {
-    //             knex('users').where('email', decoded.email).first()
-    //                 .then((user) => {
-    //                     if (user) {
-    //                         req.user = user;
-    //                         next()
-    //                     } else {
-    //                         next(new Error('You must log in'))
-    //                     }
-    //                 })
-    //         }
-    //     })
-    // }
+}
 let isExist = (req, res, next) => {
     knex('items').where('id', req.params.id).first()
         .then((item) => {
